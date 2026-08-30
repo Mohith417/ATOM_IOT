3b56d765d0f413dfe9a38a921e65d7341815af0b
A MOHITH SANTOSH <amohith007@gmail.com>
2026-08-30 17:17:21 +0530

    Add files via upload

---
+++ ATOM-IOT/Day 9/Assignment/LeetCode/L2.java
@@ -0,0 +1,28 @@
+// ==================== #11 – Container With Most Water ====================
+
+class Solution {
+    public int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+
+            int h = Math.min(height[left], height[right]);
+            int w = right - left;
+            int water = h * w;
+
+            if (water > maxWater) {
+                maxWater = water;
+            }
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+}
